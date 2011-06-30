73850cb8e62fdab9da7b2f89160b9f6c9bc973a2
Dave Hoover <dave.hoover@gmail.com>
2011-06-30 17:59:59 -0500

    Some small progress toward stronger tests, still needs more power

--- spec/courses-spec.js
+++ spec/courses-spec.js
@@ -8,29 +8,42 @@ var testRedis = function() {
   return redis.createClient(53535);
 };
 
-var testCourseName = "Thoreau in the 21st Century";
+var testCourseNames = ["Thoreau in the 21st Century", "13th CenturyMongolian Art"];
+
+var clearData = function() {
+  var client = testRedis();
+  var callback = this.callback;
+  courses.deleteAll(client, function() {
+    client.quit();
+    callback();
+  });
+};
+
+var courseCounter = 0;
+var newCourse = function() {
+  var client = testRedis();
+  var callback = this.callback;
+  courses.create(client, {name: testCourseNames[courseCounter++]}, function() {
+    client.quit();
+    callback();
+  });
+}
+
 var setupBatch = function(tests) {
   var fixtureData = {
     '': {
-      topic: function() {
-        var client = testRedis();
-        var callback = this.callback;
-        courses.deleteAll(client, function() {callback(null, client)});
-      },
+      topic: clearData,
       '': {
-        topic: function(client) {
-          var callback = this.callback;
-          courses.create(client, {name: testCourseName}, function() {
-            client.quit();
-            callback();
-          });
+        topic: newCourse,
+        '': {
+          topic: newCourse
         }
       }
     }
   };
   
   for (var label in tests) {
-    fixtureData[''][''][label] = tests[label];
+    fixtureData[''][''][''][label] = tests[label];
   }
   
   return fixtureData;
@@ -42,7 +55,7 @@ vows.describe('courses').addBatch(setupBatch({
       courses.all(testRedis, this.callback);
     },
     'should provide all 2 courses': function (courses) {
-      assert.equal(courses.length, 1);
+      assert.equal(courses.length, 2);
     }
   },
   find: {
@@ -50,7 +63,7 @@ vows.describe('courses').addBatch(setupBatch({
       courses.find(testRedis, 1, this.callback);
     },
     'should provide a course': function (course) {
-      assert.equal(course.name, testCourseName);
+      assert.equal(course.name, testCourseNames[0]);
     }
   }
 })).run();
