f0c51e77f75a243c6058e2f0d97dd805457d7364
Dave Hoover <dave.hoover@gmail.com>
2011-07-06 01:11:13 -0500

    Catching sandbox.js up with what's what

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,22 @@
-redis = require("redis")
-sys = require("sys")
-courses = require("./courses")
+redis = require("redis");
+sys = require("sys");
+Courses = require("./courses").Courses;
 
 client = redis.createClient();
+courses = new Courses(client);
 
 process.on('uncaughtException', function(e) {
   console.log("Something unawesome happened: " + e.message);
   client.quit();
 });
 
-courses.deleteAll(client, function() {
-  courses.create(client, {name: "Thoreau in the 21st Century"}, function() {
-    courses.all(client, function(courses) {
-      console.log(courses)
-      client.quit();
+courses.deleteAll(function() {
+  courses.create({name: "Thoreau in the 21st Century"}, function() {
+    courses.create({name: "13th Century Mongolian Art"}, function() {
+      courses.all(function(error, courses) {
+        console.log(courses)
+        client.quit();
+      });
     });
   });  
 });
