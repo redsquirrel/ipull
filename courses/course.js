b0f4493332440fcb2b8bed4b1e1d8d91767b334d
Jose Dominguez <domingjj@tcd.ie>
2011-09-03 18:20:40 +0100

    added logic to show number of available seats in course and handling enrollment

--- courses/course.js
+++ courses/course.js
@@ -5,6 +5,14 @@ module.exports = Course = function(id) {
     return this["min-learners"] - learners.length;
   };
   
+  this.learnersSeatsAvailable = function(learners) {
+    return this["max-learners"] - learners.length;
+  };
+
+  this.learnersMaxed = function(learners) {
+    return this["max-learners"] <= learners.length;
+  };
+
   this.timeToJoin = function(now) {
     return (new Date(this["decision-date"] )) - currentEpochTime(now);
   };
